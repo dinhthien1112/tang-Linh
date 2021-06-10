b3a59ee58883840f1b8f67fb11c1b2b9c0d4f988
dinhthien1112 <85704696+dinhthien1112@users.noreply.github.com>
2021-06-11 02:11:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
-    introTitle: 'Linh à!',
+    introTitle: 'Linh à :)))))',
     introDesc: `Trái đất vốn lạ thường
     Mà sao Linh cứ đi nhầm đường
     Lạc vào tim Thiện lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Thiện đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Thiện đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Linh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Linh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Chắc chắn là thích Thiện <33',
     btnNo: 'Không, Bạn bè à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao Linh lại thích Thiện :))))',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Thiện biết mà 🥰. Iu Linh nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h Thiện qua đón Linh đi ăn bún đậu haaaa :))).',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/dinhthien15
 }
